f234a84297828cbabd68bcdc1295c6ae9b379e43
George Paul <gpaul.empyrean@gmail.com>
2015-08-22 16:57:43 -0400

    Add methods to card model
    ask the card if it has any guesses as well as if it has any incorrect guesses.

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,4 +1,12 @@
 class Card < ActiveRecord::Base
   belongs_to :deck
   has_many :guesses
+
+  def has_incorrect_guesses?
+    guesses.select {|guess| guess.incorrect?}
+  end
+
+  def has_no_guesses?
+    guesses.empty?
+  end
 end
