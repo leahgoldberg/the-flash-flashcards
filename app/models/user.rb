2844d3a5e1fca0204c660a06ce426b2f6dc5a14d
name": null <email": null>
2015-08-22 11:00:17 -0400

    Authentication method added to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,4 +14,13 @@ class User < ActiveRecord::Base
     self.password_hash = @password
   end
 
+  def self.authenticate(username, password)
+    user = User.find_by(username: username)
+    if user.password == password
+      return user
+    else
+      return nil
+    end
+  end
+
 end
