89cf6f7116b4d850392bc4be647b5fca200322b8
George Paul <gpaul.empyrean@gmail.com>
2015-08-22 17:53:26 -0400

    Add bang to save method

--- app/controllers/rounds.rb
+++ app/controllers/rounds.rb
@@ -1,7 +1,7 @@
 post '/rounds' do
   @round = Round.new(params[:round])
   @round.player = current_user
-  if @round.save
+  if @round.save!
     redirect "/round/#{@round.id}"
   else
     redirect "/"
