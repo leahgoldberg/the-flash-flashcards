57642197be249bd80d7560b7d5e6aade781d7858
name": null <email": null>
2015-08-22 11:25:18 -0400

    Submitting complete signup form successfully creates new user

--- app/controllers/users.rb
+++ app/controllers/users.rb
@@ -3,14 +3,29 @@ get "/users/signin" do
 end
 
 
+get "/users/new" do
+  erb :"/users/new"
+end
 
+# put "/users/signin" do
+#   user = User.authenticate(params[:username], params[:password])
+#   if user
+#     sessions[:user_id] = user.id
+#     "Happy"
+#   else
+#     "Sad"
+#   end
+# end
 
-put "/users/signin" do
-  user = User.authenticate(params[:username], params[:password])
-  if user
-    sessions[:user_id] = user.id
-    redirect '/'
+post "/user" do
+  @user = User.new(params[:user])
+  if @user.save
+    # session[:user_id] = @user.id
+    # redirect "/"
+    "HAPPY"
   else
-    redirect '/'
+    # flash[:error] = @user.errors.full_messages
+    # redirect "/users/new"
+    "SAD"
   end
 end
