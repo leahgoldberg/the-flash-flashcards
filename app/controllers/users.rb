3c07de9ef0252279b13cfefa580a4c83ff73b959
name": null <email": null>
2015-08-22 11:02:10 -0400

    redirect to sign in form in users controller

--- app/controllers/users.rb
+++ app/controllers/users.rb
@@ -1,4 +1,16 @@
 get "/users/signin" do
+  erb :"users/signin"
+end
 
 
+
+
+put "/users/signin" do
+  user = User.authenticate(params[:username], params[:password])
+  if user
+    sessions[:user_id] = user.id
+    redirect '/'
+  else
+    redirect '/'
+  end
 end
