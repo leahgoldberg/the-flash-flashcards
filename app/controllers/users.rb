052451a7aa6c5ca32c67959831dd1be21029d62d
name": null <email": null>
2015-08-22 11:36:20 -0400

    Sign in with valid information starts session

--- app/controllers/users.rb
+++ app/controllers/users.rb
@@ -7,22 +7,21 @@ get "/users/new" do
   erb :"/users/new"
 end
 
-# put "/users/signin" do
-#   user = User.authenticate(params[:username], params[:password])
-#   if user
-#     sessions[:user_id] = user.id
-#     "Happy"
-#   else
-#     "Sad"
-#   end
-# end
+put "/users/signin" do
+  user = User.authenticate(params[:username], params[:password])
+  if user
+    session[:user_id] = user.id
+    redirect "/"
+  else
+    "Sad"
+  end
+end
 
 post "/user" do
   @user = User.new(params[:user])
   if @user.save
-    # session[:user_id] = @user.id
-    # redirect "/"
-    "HAPPY"
+    session[:user_id] = @user.id
+    # redirect "/decks"
   else
     # flash[:error] = @user.errors.full_messages
     # redirect "/users/new"
