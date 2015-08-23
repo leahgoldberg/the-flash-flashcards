4ce398adfc6db9dc26bc84a6893c0b33034f7ffe
George Paul <gpaul.empyrean@gmail.com>
2015-08-22 20:34:31 -0400

    Add new to round route
    Append new to the end of the new round route.  This was I can just call new round to show the round instead of creating some obscure route name for showing the stats.

--- app/controllers/rounds.rb
+++ app/controllers/rounds.rb
@@ -2,13 +2,13 @@ post '/rounds' do
   @round = Round.new(params[:round])
   @round.player = current_user
   if @round.save!
-    redirect "/round/#{@round.id}"
+    redirect "/round/#{@round.id}/new"
   else
     redirect "/"
   end
 end
 
-get "/round/:id" do
+get "/round/:id/new" do
   round = Round.find_by(id: params[:id])
   card = round.new_card
   redirect "/round/#{round.id}/card/#{card.id}"
