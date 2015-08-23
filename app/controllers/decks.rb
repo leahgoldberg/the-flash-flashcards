29f5575afdb059873162fc98cb21788f34d177af
name": null <email": null>
2015-08-23 12:18:59 -0400

    create link and route for creating a new deck

--- app/controllers/decks.rb
+++ app/controllers/decks.rb
@@ -4,9 +4,14 @@ get '/decks' do
   erb :'/welcome/index'
 end
 
+get '/decks/new' do
+  erb :'/decks/new'
+end
+
 get '/decks/:id' do
   require_signin
   @deck = Deck.find_by(id: params[:id])
   @cards = @deck.cards
   erb :'/decks/show'
 end
+
