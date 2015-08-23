2a410031b6fc93f0503569558c74b5ff1ab3dff1
Kelly Ripple <email": null>
2015-08-23 14:52:22 -0400

    WIP

--- app/controllers/deck.rb
+++ app/controllers/deck.rb
@@ -5,6 +5,7 @@ end
 
 post '/decks' do
   @deck = Deck.new(params[:deck])
+  @deck.author = current_user.id
   if @deck.save
     redirect "/decks/#{@deck.id}/cards/new"
   else
@@ -16,6 +17,25 @@ end
 get '/decks' do
   @decks = Deck.all
   erb :'decks/index'
+end
+
+get "/users/decks/:id/edit" do
+  @deck = Deck.find_by(id: params[:id])
+  if @deck.valid?
+    erb :'/decks/edit'
+  else
+    flash[:errors] = @deck.errors.full_messages
+  end
+end
+
+get '/users/decks' do
+  @user = current_user
+  if logged_in?
+    @decks = Deck.authored_by(@user.id)
+    erb :'users/decks'
+  else
+    redirect '/users/login'
+  end
 end
 
 
