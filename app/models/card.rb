846f941b6db67a1ffd8d16dd3717dece4662b6af
name": null <email": null>
2015-08-23 15:33:49 -0400

    Card can't be added to deck without question and answer

--- app/models/card.rb
+++ app/models/card.rb
@@ -2,6 +2,9 @@ class Card < ActiveRecord::Base
   belongs_to :deck
   has_many :guesses
 
+  validates :question, presence: true
+  validates :answer, presence: true
+
   def last_guess_incorrect?(current_round)
     current_round_guesses(current_round).last.incorrect?
   end
