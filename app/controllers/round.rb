2bf5831b5c40cea9f4b374aebfbf8f3cfce0754f
leahgoldberg <leahgoldberg31@gmail.com>
2015-08-23 09:29:55 -0400

    add new controllers for rounds and guesses

--- app/controllers/round.rb
+++ app/controllers/round.rb
@@ -1,19 +1,12 @@
-get '/decks' do
-  @decks = Deck.all
-  erb :'decks/index'
-end
-
 get '/decks/:id/rounds/new' do
   @deck = Deck.find_by(id: params[:id])
-  @game = Game.new(deck: @deck, user: current_user)
-  if @deck && @game.save
+  @round = Round.new(deck: @deck, user: current_user)
+  if @deck && @round.save
     @deck.reset_cards!
+    @card = @deck.random_card
     redirect "rounds/#{@round.id}/cards/#{@card.id}"
   else
     status 404
     erb :"errors/not_found"
   end
 end
-
-
-
