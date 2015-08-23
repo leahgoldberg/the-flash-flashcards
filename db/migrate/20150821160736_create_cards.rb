317f6d74b5fb4f5ff209211d542de15fc1a7f420
Kelly Ripple <email": null>
2015-08-23 00:16:40 -0400

    Add card column 'used' to track used cards

--- db/migrate/20150821160736_create_cards.rb
+++ db/migrate/20150821160736_create_cards.rb
@@ -3,7 +3,9 @@ class CreateCards < ActiveRecord::Migration
     create_table :cards do |t|
       t.text :question, null:false
       t.string :answer, null:false
+      t.boolean :used
       t.references :deck
+      t.references :round
       t.timestamps
     end
   end
